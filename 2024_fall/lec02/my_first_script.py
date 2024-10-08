c201d169366d1faa374eff9c3c5c0f810a219625
yanguiyou <ketaclo@gmail.com>
2024-10-08 10:58:18 +0900

    Fall2024_SpeechUnderstannding_M24W0597

---
+++ 2024_fall/lec02/my_first_script.py
@@ -0,0 +1,19 @@
+""" This is a script that I am creating as part of lecture 2 in the Speech Understanding course. """
+
+print("Hello world、キェブ　です。")
+
+print('5+6 is',5+6)
+print(9-4)
+print(9/3)
+print(5**3)
+print(13%5)
+print(13//5)
+
+name1 = "KCGI"
+name2 = "Kyoto"
+
+print(name1)
+
+print(type(name1))
+
+print(name1 + " is in " + name2*3 )
